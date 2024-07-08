ea8935d3e409aca6e766f1d2f6c3dc63b5c271bd
Apoorva <113757327+jha-apoorva@users.noreply.github.com>
2024-07-08 11:23:13 +0530

    Time: 4 ms (65.65%), Space: 40.3 MB (40%) - LeetHub

---
+++ 0856-consecutive-numbers-sum/0856-consecutive-numbers-sum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int consecutiveNumbersSum(int n) {
+        int count = 0;
+        for(int k=1;k*(k-1)<2*n;k++){
+            if((n-(k*(k-1))/2)%k == 0){
+                count++;
+            }           
+        }
+        return count;
+        
+    }
+}
