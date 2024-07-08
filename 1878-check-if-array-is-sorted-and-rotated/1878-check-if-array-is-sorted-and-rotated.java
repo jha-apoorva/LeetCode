416907ab65f3a6dccb095226ca96bee45e96f3b2
Apoorva <113757327+jha-apoorva@users.noreply.github.com>
2024-07-08 09:50:42 +0530

    Time: 0 ms (100%), Space: 41.8 MB (6.78%) - LeetHub

---
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int isInc(int[] nums){
+        int pivot=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] < nums[i-1]){
+                pivot = i;
+                break;
+            }
+        }
+    return pivot;
+    }
+    public boolean check(int[] nums) {
+        int p = isInc(nums);
+        int[] arr = new int[nums.length];
+        int n = arr.length;
+        for(int i=0; i<n;i++){
+            arr[i] = nums[(i+p) % n];
+            }
+        if(isInc(arr)==0)
+        return true;
+        else
+        return false;     
+    }
+}
