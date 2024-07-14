32149818aa7bc2d0dfd33aec3118bda11cbe47da
Apoorva <113757327+jha-apoorva@users.noreply.github.com>
2024-07-14 10:33:00 +0530

    Time: 1 ms (39.91%), Space: 45.3 MB (30.94%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
+        /*int n = nums.length;
         // 0 - n numbers will be present if length would be n+1
          Arrays.sort(nums);
          int x = n;
@@ -10,6 +10,24 @@ class Solution {
                 break;
             }
          }
-    return x;
+    return x;*/
+
+    int i=0;
+    while(i < nums.length){
+        int correct = nums[i];
+        if(nums[i] < nums.length && nums[i] != nums[correct]){
+            int temp = nums[i];
+            nums[i] = nums[correct];
+            nums[correct] = temp;
+        }
+        else {
+            i++;
+        }
+    }
+    for(int j=0; j< nums.length;j++){
+        if(nums[j] != j)
+        return j;
+    }
+    return nums.length;
     }
 }
