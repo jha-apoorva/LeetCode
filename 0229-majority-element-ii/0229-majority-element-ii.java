db2424154b03fce62d46b2b33180178450b464e3
Apoorva <113757327+jha-apoorva@users.noreply.github.com>
2024-07-18 04:39:13 +0530

    Time: 12 ms (27.9%), Space: 47.3 MB (89.1%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,18 +3,14 @@ class Solution {
         HashMap<Integer,Integer> frequency = new HashMap<>();
         List<Integer> arr = new ArrayList<>();
         for(int i : nums){
-            if(frequency.containsKey(i)){
+            if(frequency.containsKey(i))
                 frequency.put(i,frequency.get(i)+1);
-            }
-            else{
+            else
                 frequency.put(i,1);
-            }
         }
-
         for(int i : frequency.keySet()){
-            if(frequency.get(i) > Math.floor(nums.length/3)){
+            if(frequency.get(i) > Math.floor(nums.length/3))
                 arr.add(i);
-            }
         }
     return arr;
         
