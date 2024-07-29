0ecdb935a5a88955b867da0ec4cc26c52dbef3a6
Apoorva <113757327+jha-apoorva@users.noreply.github.com>
2024-07-29 10:11:02 +0530

    Time: 2063 ms (8.44%), Space: 42.3 MB (52.18%) - LeetHub

---
+++ 1511-count-number-of-teams/1511-count-number-of-teams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numTeams(int[] rating) {
+        int numTeams =0;
+       for(int i=0;i<rating.length-2;i++){
+        for(int j=i;j<rating.length-1;j++){
+            for(int k=j;k<rating.length;k++){
+                if((rating[i] < rating[j] && rating[j] < rating[k]) || (rating[i] > rating[j] && rating[j] > rating[k])){
+                    numTeams++;
+                }
+            }
+        }
+       }
+    return numTeams;
+    }
+}
