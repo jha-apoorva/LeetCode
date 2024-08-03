d5266cc1e06faf656e0a4821f363da02247f12e4
Apoorva <113757327+jha-apoorva@users.noreply.github.com>
2024-08-03 19:13:21 +0530

    Time: 0 ms (100%), Space: 42.4 MB (14.15%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -8,15 +8,10 @@ class Solution {
         int j=n-1;
         int k=m+n-1;
         while(j>=0){
-            if(i>=0 && nums1[i] > nums2[j]){
-                nums1[k] = nums1[i];
-                i--;
-            }
-            else{
-                nums1[k] = nums2[j];
-                j--;
-            }
-        k--;
+            if(i>=0 && nums1[i] > nums2[j])
+                nums1[k--] = nums1[i--];
+            else
+                nums1[k--] = nums2[j--];
         }
     }
 }
