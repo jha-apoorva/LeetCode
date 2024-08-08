ce42b7d1e7d6500b8bb9f4e7d9edfcc774cd94d1
Apoorva <113757327+jha-apoorva@users.noreply.github.com>
2024-08-08 15:49:42 +0530

    Time: 1 ms (63.32%), Space: 40.4 MB (70.58%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isHappy(int n) {
         int sum=0;
-        for(int i=0;i<100;i++){
+        for(int i=0;i<50;i++){
             while(n > 0){
             int digit = n%10;
             sum += digit*digit;
