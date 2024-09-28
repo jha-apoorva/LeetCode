d2c3bb90e3421701f2c7202de2cadf9767cb6e7e
Apoorva <113757327+jha-apoorva@users.noreply.github.com>
2024-09-28 11:26:38 +0530

    Time: 1 ms (99.52%), Space: 44.9 MB (63.02%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> arr = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3 == 0 && i%5 == 0) arr.add("FizzBuzz");
+            else if(i%3==0) arr.add("Fizz");
+            else if(i%5==0) arr.add("Buzz");
+            else arr.add(String.valueOf(i));
+        }
+    return arr;    
+    }
+}
