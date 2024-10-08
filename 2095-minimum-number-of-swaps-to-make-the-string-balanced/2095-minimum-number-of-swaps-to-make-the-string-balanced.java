ddefc6e7f3e0b96ae9940674beda429b7613603b
Apoorva <113757327+jha-apoorva@users.noreply.github.com>
2024-10-08 12:24:15 +0530

    Time: 14 ms (76.82%), Space: 53.7 MB (95.33%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -3,15 +3,13 @@ class Solution {
         int openBrackets = 0, closeBrackets = 0;
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if(ch =='['){
+            if(ch ==']'){
                 openBrackets++;
-            }
-            else if(openBrackets<=0){
-                closeBrackets++;
             }
             else{
                 openBrackets--;
             }
+            closeBrackets = Math.max(openBrackets,closeBrackets);
         }
         return (closeBrackets+1)/2;
     }
