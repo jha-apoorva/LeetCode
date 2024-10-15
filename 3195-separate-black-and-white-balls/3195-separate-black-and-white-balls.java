877f35c23015cbbd22a8e140bcdc5976373fd136
Apoorva <113757327+jha-apoorva@users.noreply.github.com>
2024-10-15 21:10:00 +0530

    Time: 7 ms (100%), Space: 45.5 MB (30.47%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -1,11 +1,18 @@
 class Solution {
     public long minimumSteps(String s) {
-        int black = 0;
+        /*int black = 0;
         long ans = 0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i) == '0') ans += (long) black;
             else black++;
         }
+    return ans;*/
+    long white = 0;
+    long ans=0;
+    for(int i=s.length()-1;i>=0;i--){
+        if(s.charAt(i) == '0') white++;
+        else ans += white;
+    }
     return ans;
     }
 }
