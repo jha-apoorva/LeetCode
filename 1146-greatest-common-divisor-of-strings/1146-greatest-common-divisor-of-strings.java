c85fc600d98a16a3a6793411add4ea35388e830d
Apoorva <113757327+jha-apoorva@users.noreply.github.com>
2024-10-19 22:23:15 +0530

    Time: 0 ms (100%), Space: 42.3 MB (25.62%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -1,14 +1,10 @@
 class Solution {
+    public int gcd(int a,int b){
+        return b==0?a:gcd(b,a%b);
+    }
     public String gcdOfStrings(String str1, String str2) {
-        if(str2.length() > str1.length()){
-            return gcdOfStrings(str2,str1);
-        }
-        if(str2.equals(str1)){
-            return str2;
-        }
-        if(str1.startsWith(str2)){
-            return gcdOfStrings(str1.substring(str2.length()),str2);
-        }
-        return "";
+        if(!(str1+str2).equals(str2+str1)) return "";
+        int gcd = gcd(str1.length(),str2.length());
+        return str1.substring(0,gcd);
     }
 }
