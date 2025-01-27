02d16d5ff7f02b63ad838878a2ddf52e206d44d3
Apoorva <113757327+jha-apoorva@users.noreply.github.com>
2025-01-27 17:30:01 +0530

    Time: 0 ms (100%), Space: 43.1 MB (45.27%) - LeetHub

---
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for(String str : words){
+            if(str.startsWith(pref)) count++;
+        }
+    return count;
+    }
+}
